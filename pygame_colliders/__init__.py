a4928882876b1721c1cb0cf9049b89902a94f6a1
Jani Tiainen <jani@tiainen.cc>
2020-10-15 23:14:30 +0300

    Changed from pipenv to poetry.

--- pygame_colliders/__init__.py
+++ pygame_colliders/__init__.py
@@ -3,5 +3,9 @@ from .convex import ConvexCollider
 from .rect import Rect
 from .vector import Vector2
 
-VERSION = (0, 1, 3, "dev1")
-__version__ = ".".join([str(x) for x in VERSION])
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
